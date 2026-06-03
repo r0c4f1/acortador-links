bd376198b5480f19282ea45913c7864b884d731d
Roberto <rducharne2@gmail.com>
2026-06-03 19:49:37 -0400

    cambio de bd

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import { DATABASE_URL } from "../enviroment"
 
 
 export const pool = new Pool({
-  connectionString: DATABASE_URL,
+  connectionString: "postgresql://neondb_owner:npg_CxzFGcBQ6T5m@ep-proud-credit-apyqvr80.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require",
   ssl: {
     rejectUnauthorized: false,
   }
